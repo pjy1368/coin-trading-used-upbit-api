2391ef80b274978eed4241d7aaf4ee97af87f21c
JinYoung Park <basejin0810@naver.com>
2021-06-01 10:43:16 +0900

    init: 초기 설정

---
+++ src/main/java/com/wooteco/upbit/UpbitApplication.java
@@ -0,0 +1,13 @@
+package com.wooteco.upbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitApplication.class, args);
+	}
+
+}
